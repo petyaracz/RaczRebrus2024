f3c15f0cd01b964d27d22200faece72c0c8b943a
Peter Racz <pracz@cogsci.bme.hu>
2024-02-08 16:43:46 +0100

    hmmm

--- scripts/knn.R
+++ scripts/knn.R
@@ -7,6 +7,7 @@ library(glue)
 library(magrittr)
 library(stringdist)
 library(furrr)
+library(patchwork)
 
 no_cores <- availableCores() - 1
 plan(multisession, workers = no_cores)
@@ -24,20 +25,18 @@ transcribeIPA = function(string, direction){
 }
 
 # take frequency ntile (1-10), form, k, return back weight for form
-KNN = function(my_ntile,my_form,k){
-  
-  # tr form
-  my_form_ipa = transcribeIPA(my_form, 'single')
+KNN = function(dat,my_ntile,my_frm,my_k){
   
   # find the stem that belongs to the form
-  my_stem = d %>% 
-    filter(form == my_form) %>% 
+  my_stem = dat %>% 
+    filter(frm == my_frm) %>% 
     distinct(stem) %>% 
     pull()
   
-  # take d, set up ntiles
-  relevant_forms = d %>% 
+  # take dat, set up ntiles
+  relevant_forms = dat %>% 
     mutate(
+      freq = back + front,
       ntile_freq = ntile(freq, 10) 
     ) %>% 
     # filter for freq ntile, drop forms belonging to target form stem
@@ -47,77 +46,97 @@ KNN = function(my_ntile,my_form,k){
     ) %>%
     # get LV dist for each form and target form
     mutate(
-      form_ipa = transcribeIPA(form, 'single'),
-      dist = stringdist(my_form_ipa,form_ipa, method = 'lv')    
+      dist = stringdist(my_frm, frm, method = 'lv')    
     ) %>% 
     # get all nearest neighbours in min dist - k range
-    filter(dist == min(dist) - 1 + k) %>% 
-    select(form,stem,dist,freq,suffix_vowel)
+    filter(dist == min(dist) - 1 + my_k)
   
   weights = relevant_forms %>% 
     # get sum freqs across front and back vowel forms
-    group_by(suffix_vowel) %>% 
-    summarise(weight = sum(freq))
+    summarise(
+      back = sum(back),
+      front = sum(front)
+      ) %>%
+    # get prop back
+    mutate(back_weight = back / (back + front))
   
-  # we might end up with a setup where all matching forms are front or back so we pull these into vars (and if that one row doesn't exist the var will be 0)
-  front = weights %>% 
-    filter(suffix_vowel == 'front')
-  back = weights %>% 
-    filter(suffix_vowel == 'back')
-  
-  n_front = pull(front, weight)
-  n_back = pull(back, weight)
-  
-  has_front = nrow(front) > 0
-  has_back = nrow(back) > 0
-  
-  # we calc back weight as a proportion, not an odds
-  # you can't do this with a case_when because that evaluates everything in one go and then picks the right answer. but if one of the alternatives is 0/something then you get a numeric0 and then everything is numeric0. you need an if switch that goes through alternatives one after the other.
-  if(!has_back){
-      back_weight = 0
-    } else if(!has_front){
-      back_weight = 1
-    } else {
-      back_weight = n_back / (n_back + n_front)
-    }
-
-  return(back_weight)
+  # pull weight
+  weights %>% 
+    pull(back_weight)
 }
 
 # take dat, ntile and k and map knn across dat, return fit on accuracy of weights
 mapKNN = function(dat,my_ntile,my_k){
   # we take the KNN fun and apply it to every form in ALPHABETIC ORDER
-  d2 = dat %>% 
+  dat2 = dat %>% 
     mutate(
-      weight = future_map_dbl(form, ~ KNN(my_ntile,.,my_k))
+      weight = future_map_dbl(frm, ~ KNN(dat,my_ntile,.,my_k))
     )
   
-  # we get freq pairs per suffixed stem for front back forms
-  freq_pairs = d2 %>% 
-    group_by(stem,suffix,suffix_vowel) %>% 
-    summarise(freq = sum(freq)) %>% 
-    pivot_wider(names_from = suffix_vowel, values_from = freq, values_fill = 0) %>% 
-    ungroup()
+  return(dat2)
+}
+
+# map mapKNN through the pars for w or c, return best model w/ preds and glm est
+runPars = function(my_dat){
   
-  # we do the same for form back weights
-  weight_pairs = d2 %>% 
-    group_by(stem,suffix,suffix_vowel) %>% 
-    summarise(weight = mean(weight)) %>% 
-    pivot_wider(names_from = suffix_vowel, values_from = weight, values_fill = 0) %>% 
-    mutate(back_weight = back / (front + back)) %>% 
-    select(stem,suffix,back_weight) %>% 
-    ungroup()
+  knns = pars %>% 
+    mutate(
+      pred = map2(ntile,k, ~ mapKNN(dat = my_dat, my_ntile = .x, my_k = .y)) 
+    )
   
-  # we join the two and scale the avgd weight
-  d3 = left_join(freq_pairs, weight_pairs, by = join_by(stem, suffix)) %>% 
-    select(stem,suffix,front,back,back_weight)
+  # fit glm to see how accurate the preds are
+  accs = knns %>% 
+    mutate(
+      pred2 = map(pred, ~ mutate(., scaled_weight = scale(weight))),
+      fit = map(pred2, ~ glm(cbind(back,front) ~ 1 + scaled_weight, family = binomial, data = .)),
+      est = map(fit, ~ broom::tidy(., conf.int = T))
+    ) %>% 
+    select(ntile,k,est) %>% 
+    unnest(est)
   
-  return(d3)
+  # find the best one
+  best = accs %>% 
+    filter(
+      term == 'scaled_weight',
+      statistic == max(statistic)
+    )
+  
+  best = knns %>% 
+    inner_join(best) %>% 
+    unnest(pred)
+  
+  return(best)
 }
 
 # -- read -- #
 
-d = read_tsv('dat/dat_long.tsv')
+w = read_tsv('dat/dat_wide.tsv')
+l = read_tsv('dat/dat_long.tsv')
+
+# -- wrangle -- #
+
+vowel = '[aáeéiíoóöőuúüű]'
+
+# get dense target forms into w
+w = l %>% 
+  mutate(
+    frm = form %>% 
+      transcribeIPA('single') %>% 
+      str_replace_all(vowel, '')
+  ) %>% 
+  distinct(stem,suffix,frm) %>% 
+  right_join(w)
+
+c = filter(w, form_varies)
+
+s = c %>% 
+  group_by(stem) %>% 
+  summarise(
+    back = sum(back),
+    front = sum(front)
+  ) %>% 
+  mutate(frm = transcribeIPA(stem, 'single')) %>% 
+  ungroup()
 
 # -- main -- #
 
@@ -126,21 +145,34 @@ d = read_tsv('dat/dat_long.tsv')
 ## all forms
 
 # get predictions for various parameter settings
-knns = crossing(
+pars = crossing(
   ntile = c(1,5,9),
   k = 1:4
-) %>% 
-  mutate(
-    pred = map2(ntile,k, ~ mapKNN(dat = d, my_ntile = .x, my_k = .y)) 
-  )
+) 
 
-# fit glm to see how accurate the preds are
-knns %>% 
-  mutate(
-    pred2 = map(pred, ~ mutate(., scaled_weight = scale(back_weight))),
-    fit = map(pred2, ~ glm(cbind(back,front) ~ 1 + scaled_weight, family = binomial, data = .)),
-    est = map(fit ~ broom::tidy(., conf.int = T) %>% 
-                    filter(term == 'scaled_weight') %>% 
-                    select(-term)
-    )
-  )
+best_w = runPars(w)
+best_c = runPars(c)
+best_s = runPars(s)
+best_s = best_s %>% 
+  select(stem,weight) %>% 
+  left_join(c)
+
+p1 = best_w %>% 
+  mutate(p = back / (back + front)) %>% 
+  ggplot(aes(weight,p)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+p2 = best_c %>% 
+  mutate(p = back / (back + front)) %>% 
+  ggplot(aes(weight,p)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+p3 = best_s %>% 
+  mutate(p = back / (back + front)) %>% 
+  ggplot(aes(weight,p)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+p1 + p2 + p3
